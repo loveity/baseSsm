fa5e44936b732e4bcc1e6c80d0c7ab7868fbafa6
loveity <justloveit24@sina.com>
2017-11-15 11:06:50 +0800

    ti j SSM 基础框架 Signed-off-by: loveity <justloveit24@sina.com>

---
+++ src/main/java/com/wqq/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.wqq.controller;
+
+import com.wqq.dao.UUserMapper;
+import com.wqq.po.UUser;
+import com.wqq.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    @Resource
+    UserService userService;
+
+//    @ResponseBody
+    @RequestMapping("/user")
+    public String find(Model model) {
+        model.addAttribute("ss",userService.find());
+        return "user";
+    }
+
+    @RequestMapping("/sign")
+    public String sign() {
+        return "sign";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/user.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2017/10/19
+  Time: 3:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${ss}
+</br>
+  <a onclick="javaScript:history.Go(-1)">返回   </a>
+</body>
+</html>
